774c8292d6f10d8db82467c7b32d20dfcccfb78e
RubenAcosta16 <1511ruben@gmail.com>
2023-03-21 23:00:18 -0600

    aaa

--- src/components/dashboardWrapper.jsx
+++ src/components/dashboardWrapper.jsx
@@ -1,17 +1,19 @@
-import { useNavigate, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
+// import style from "./dashboardWrapper.module.css";
 
-const dashBoardWrapper = ({children}) => {
-    return <div>
-        <nav>
-            <div>Logo</div>
-            <Link to="/dashboard">Links</Link>
-            <Link to="/dashboard/profile">Profile</Link>
-            <Link to="/signout">Sign out</Link>
+export default function dashboardWrapper({ children }) {
+  return (
+    <div>
+      <nav className={style.nav}>
+        <div className={style.logo}>Logotipo</div>
+        <Link to="/dashboard">Links</Link>
 
-        </nav>
-        <div>{children}</div>
-    </div>;
+        <Link to="/dashboard/profile">Profile</Link>
+
+        <Link to="/signout">Sign out</Link>
+      </nav>
+
+      <div className="main-container">{children}</div>
+    </div>
+  );
 }
-
-
-export default dashBoardWrapper;
